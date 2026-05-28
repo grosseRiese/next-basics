943666e56ff22930b11ae25917cd25b0dca593af
grosseRiese <plmman2010@gmail.com>
2026-05-28 16:00:30 +0200

    post-page, and card

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,23 +6,11 @@ export default async function PostsPage() {
   return (
     <div className="container p-4">
       <h1 className="mb-4 text-2xl font-bold">Posts </h1>
-      <ul className="space-y-2">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
         {posts.map((post) => (
           <PostCard key={post.id} post={post} />
-          //   <li key={post.id}>
-          //     <span className="font-semibold">{post.title}</span>
-          //     <span className="ml-2 text-sm text-gray-600">
-          //       by {post.content}
-          //     </span>
-          //     <span className="ml-2 text-sm text-gray-500">
-          //       (Created At: {post.createdAt.toLocaleDateString()})
-          //     </span>
-          //     <span className="ml-2 text-sm text-gray-500">
-          //       (updated At: {post.updatedAt.toLocaleDateString()})
-          //     </span>
-          //   </li>
         ))}
-      </ul>
+      </div>
     </div>
   )
 }
--- components/post-card.tsx
+++ components/post-card.tsx
@@ -51,17 +51,6 @@ function PostCard({ post }: Props) {
         </Button>
       </CardFooter>
     </Card>
-
-    //     <li key={post.id}>
-    //       <span className="font-semibold">{post.title}</span>
-    //       <span className="ml-2 text-sm text-gray-600">by {post.content}</span>
-    //       <span className="ml-2 text-sm text-gray-500">
-    //         (Created At: {post.createdAt.toLocaleDateString()})
-    //       </span>
-    //       <span className="ml-2 text-sm text-gray-500">
-    //         (updated At: {post.updatedAt.toLocaleDateString()})
-    //       </span>
-    //     </li>
   )
 }
 
