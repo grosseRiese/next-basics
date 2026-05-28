3cd12b8f53f63f37830e586c0a29d2cf7650d04b
grosseRiese <plmman2010@gmail.com>
2026-05-28 15:52:52 +0200

    post card, posts rendered

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,28 @@
-export default function PostsPage() {
+import { PostCard } from "@/components/post-card"
+import prisma from "@/lib/prisma"
+
+export default async function PostsPage() {
+  const posts = await prisma.post.findMany({})
   return (
     <div className="container p-4">
       <h1 className="mb-4 text-2xl font-bold">Posts </h1>
-      <p> Learn how to fetch posts from the database and display them here.</p>
+      <ul className="space-y-2">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} />
+          //   <li key={post.id}>
+          //     <span className="font-semibold">{post.title}</span>
+          //     <span className="ml-2 text-sm text-gray-600">
+          //       by {post.content}
+          //     </span>
+          //     <span className="ml-2 text-sm text-gray-500">
+          //       (Created At: {post.createdAt.toLocaleDateString()})
+          //     </span>
+          //     <span className="ml-2 text-sm text-gray-500">
+          //       (updated At: {post.updatedAt.toLocaleDateString()})
+          //     </span>
+          //   </li>
+        ))}
+      </ul>
     </div>
   )
 }
---
+++ components/post-card.tsx
@@ -0,0 +1,68 @@
+import type { Post } from "@/generated/prisma/client"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import { MoveRight } from "lucide-react"
+
+type Props = {
+  post: Post
+  // OR ...
+  //   post: {
+  //     id: number;
+  //     title: string;
+  //     content: string;
+  //     createdAt: Date;
+  //     updatedAt: Date;
+  //   }
+}
+function PostCard({ post }: Props) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <h2>{post.title}</h2>
+        </CardTitle>
+        <CardDescription>
+          <p className="text-sm text-gray-500">
+            Created At: {post.createdAt.toLocaleDateString()}
+          </p>
+          <p className="text-sm text-gray-500">
+            Updated At: {post.updatedAt.toLocaleDateString()}
+          </p>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p className="line-clamp-2">{post.content}</p>
+        {/*OR  <p className="">{post.content.substring(0, 100)}...</p> */}
+      </CardContent>
+      <CardFooter>
+        <Button asChild className="w-full">
+          <Link href={`/posts/${post.id}`}>
+            View post
+            <MoveRight className="ml-2" />
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
+
+    //     <li key={post.id}>
+    //       <span className="font-semibold">{post.title}</span>
+    //       <span className="ml-2 text-sm text-gray-600">by {post.content}</span>
+    //       <span className="ml-2 text-sm text-gray-500">
+    //         (Created At: {post.createdAt.toLocaleDateString()})
+    //       </span>
+    //       <span className="ml-2 text-sm text-gray-500">
+    //         (updated At: {post.updatedAt.toLocaleDateString()})
+    //       </span>
+    //     </li>
+  )
+}
+
+export { PostCard }
