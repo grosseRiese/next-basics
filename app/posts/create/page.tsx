ccfa55d1da53158026fd5d2b91a47542b520ed4a
grosseRiese <plmman2010@gmail.com>
2026-05-29 16:11:13 +0200

    Create page.tsx

---
+++ app/posts/create/page.tsx
@@ -0,0 +1,18 @@
+import prisma from "@/lib/prisma"
+import { CreatePostForm } from "./_components/create-post-form"
+
+export default function CreatePostPage(props: PageProps<"/posts/create">) {
+  //const { title, content } = props.params
+  // const post = prisma.post.create({
+  //   data: {
+  //     title,
+  //     content,
+  //   },
+  // })
+  return (
+    <div className="mx-auto max-w-prose p-4">
+      <h1 className="mb-4 text-2xl font-bold">Create Post</h1>
+      <CreatePostForm />
+    </div>
+  )
+}
