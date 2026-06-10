df8992f3abec250629c1c1add49269275e126883
grosseRiese <plmman2010@gmail.com>
2026-06-10 15:30:38 +0200

    signou compo..

--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -1,12 +1,27 @@
 "use client"
 
+import { authClient } from "@/lib/auth-client"
 import { Button } from "./ui/button"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
 
 type Props = React.ComponentProps<typeof Button>
 
 function SignOutButton(props: Props) {
-  async function handleClick() {}
-  return <Button onClick={handleClick} {...props} />
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+
+  async function handleClick() {
+    if (isLoading) {
+      return
+    }
+    setIsLoading(true)
+    await authClient.signOut()
+    router.refresh()
+    setIsLoading(false)
+  }
+
+  return <Button disabled={isLoading} onClick={handleClick} {...props} />
 }
 
 export { SignOutButton }
