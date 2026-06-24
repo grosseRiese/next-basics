42f755feffc8499120109b95dc551eac72d72975
grosseRiese <plmman2010@gmail.com>
2026-06-24 09:56:38 +0200

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,16 @@
-export default async function CartPage(){
-return(
+import { getCart } from "@/lib/cart"
+
+export default async function CartPage() {
+  const cart = await getCart()
+  const ids = Object.keys(cart)
+  //const movies = await prisma.movie.findMany({
+  // where:{
+  // id:{in:ids},deleteAt:null}
+  //});
+
+  return (
     <div>
-        <h1> Cart </h1>
+      <h1> Cart </h1>
     </div>
-)
+  )
 }
